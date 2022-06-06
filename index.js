586dec34d786a225a83f4ec242a4c3896c0c0689
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-06 21:05:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 =2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()); 
+
+const num3 = 24;
+const num4 = 5;
+const mod = 24%5
+
+const max = Math.max(2, 4, 6, 8,10, 20)
